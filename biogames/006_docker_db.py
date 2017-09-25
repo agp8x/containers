1b6ab0eacc46a82f61ed3cc7545946e03e8ab2bb
Clemens Klug <clemens.klug@uni-bamberg.de>
2017-09-25 09:20:30 +0200

    fix db-name

--- biogames/006_docker_db.py
+++ biogames/006_docker_db.py
@@ -3,7 +3,7 @@ USE_LOCAL_DB = False
 SECRET_KEY = 'longrandomstring'
 
 ADMINS = (
-	('Who Ever', 'clemens.klug@uni-bamberg.de'),
+	('Who Ever', 'is@responsib.le'),
 )
 
 DATABASES = {
@@ -11,7 +11,7 @@ DATABASES = {
 		'ENGINE':	'django.contrib.gis.db.backends.postgis',
 		'HOST':		'db', #biogames_postgis
 		'PORT':		'5432',
-		'NAME':		'biogames',
+		'NAME':		'postgres',
 		#'NAME':		'postgres',
 		'USER':		'postgres',
 		'PASSWORD':	'secret',
